eb6dc0818fc661de6f6d6abe3827c6d9fe93fb06
Victor Ochoa <vochoa.developer@gmail.com>
2021-04-07 21:03:13 -0400

    Added redirection in the case that n in AppendNTimes is <0

--- cs111-a8/cs111-a8/RecursiveAppend.java
+++ cs111-a8/cs111-a8/RecursiveAppend.java
@@ -10,6 +10,7 @@ public class RecursiveAppend {
 
     // Returns the original string appended to the original string n times 
     public static String appendNTimes (String original, int n) {
+        if(n < 0) return "Integer must be positive";
         if(n == 0) return original;
 
         return original + appendNTimes(original, n - 1);
@@ -17,8 +18,8 @@ public class RecursiveAppend {
 
     public static void main (String[] args) {
 
-        StdOut.println(appendNTimes("cat", 0));
-        StdOut.println(appendNTimes("cat", 1));
-        StdOut.println(appendNTimes("cat", 2));   
+        StdOut.println(appendNTimes("", -1));
+        StdOut.println(appendNTimes("", 1));
+        StdOut.println(appendNTimes("labyrinth", 2));   
     }
 }
