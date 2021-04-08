5e8d6405cc1fd21344eae12e7caf82ab581511cb
Victor Ochoa <vochoa.developer@gmail.com>
2021-04-07 21:00:26 -0400

    First implementation of RecursiveAppend

--- cs111-a8/cs111-a8/RecursiveAppend.java
+++ cs111-a8/cs111-a8/RecursiveAppend.java
@@ -1,8 +1,8 @@
-/*************************************************************************
+ /*************************************************************************
  *  Compilation:  javac RecursiveAppend.java
  *  Execution:    java RecursiveAppend
  *
- *  @author:
+ *  @author: Victor Ochoa
  *
  *************************************************************************/
 
@@ -10,12 +10,15 @@ public class RecursiveAppend {
 
     // Returns the original string appended to the original string n times 
     public static String appendNTimes (String original, int n) {
+        if(n == 0) return original;
 
-	// WRITE YOUR CODE HERE
+        return original + appendNTimes(original, n - 1);
     }
 
     public static void main (String[] args) {
 
-	// WRITE TEST CASES HERE to test your method
+        StdOut.println(appendNTimes("cat", 0));
+        StdOut.println(appendNTimes("cat", 1));
+        StdOut.println(appendNTimes("cat", 2));   
     }
 }
